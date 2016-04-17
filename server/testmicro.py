9632109c2ea84a53ca5c6c74a8e811890af16b2a
Akamine <kalaspa@gmail.com>
2016-04-17 23:07:37 +0200

    Nouvelle structure du projet, ajout de virtualenv

--- server/testmicro.py
+++ server/testmicro.py
@@ -12,7 +12,7 @@ def callback(recognizer, audio):
         # for testing purposes, we're just using the default API key
         # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
         # instead of `r.recognize_google(audio)`
-        print(recognizer.recognize_google(audio , language = "fr"))
+        print(recognizer.recognize_google(audio , language = "en"))
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError:
