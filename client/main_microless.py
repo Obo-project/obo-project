77ff0808c9fa139b2dc9a12889b8c7c7ca4dc14b
alexis-thual <alexisthual@gmail.com>
2016-04-19 20:31:25 +0200

    Corrections sur les comparateurs

--- client/main_microless.py
+++ client/main_microless.py
@@ -6,13 +6,7 @@ import re
 from rel_extract_obo import precompute
 from relations.hasPop import *
 
-
-UNIVERSAL = re.compile(r'.*')
-LIVE_IN = re.compile(r'.*(live|lives|inhabit|inhabits|are|is).*\bin\b(?!\b.+ing)')
-THERE = re.compile(r'.*((There.*are)|(there.*are)|(There.*is)|(there.*is))')
-IN = re.compile(r'\bin\b')
-
-sents = "There are more than 65 million people in France."
+sents = "There are 65 million people in France."
 print("Analysed sentence : ", sents)
 sents = precompute(sents)
 print("Precomputed sentence : ", sents)
--- client/relations/hasPop.py
+++ client/relations/hasPop.py
@@ -3,8 +3,9 @@ import re
 
 UNIVERSAL = re.compile(r'.*')
 LIVE_IN = re.compile(r'.*(live|lives|inhabit|inhabits|are|is).*\bin\b(?!\b.+ing)')
-THERE = re.compile(r'((There.*are)|(there.*are)|(There.*is)|(there.*is))\b')
+THERE = re.compile(r'((There.*are)|(there.*are)|(There.*is)|(there.*is))\b(?!.*(than))')
 THERE_MORE = re.compile(r'.*((There.*are)|(there.*are)|(There.*is)|(there.*is)).*(more.*than)')
+THERE_LESS = re.compile(r'.*((There.*are)|(there.*are)|(There.*is)|(there.*is)).*(less.*than)')
 IN = re.compile(r'\bin\b')
 
 number_dic = {
@@ -21,4 +22,4 @@ def make_nice(text):
     text = [replace_int(x) for x in text.split('_')]
     return " ".join(text)
 
-hasPop = relation('hasPopulation' , 'PPCD' , 'LOC' , make_nice , patterns_list=[{'left':THERE, 'middle': IN, 'comparator': 'egal'}, {'left': UNIVERSAL, 'middle': LIVE_IN, 'comparator': 'egal'}, {'left': THERE_MORE, 'middle': IN, 'comparator': 'more'}])
+hasPop = relation('hasPopulation' , 'PPCD' , 'LOC' , make_nice , patterns_list=[{'left':THERE, 'middle': IN, 'comparator': 'egal'}, {'left': UNIVERSAL, 'middle': LIVE_IN, 'comparator': 'egal'}, {'left': THERE_MORE, 'middle': IN, 'comparator': 'more'}, {'left': THERE_LESS, 'middle': IN, 'comparator': 'less'}])
