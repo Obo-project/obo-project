3be3aebd75a900f008debce9ac2abfca47759eec
alexis-thual <alexisthual@gmail.com>
2016-04-21 23:34:08 +0200

    Simplification de main

--- client/main.py
+++ client/main.py
@@ -24,18 +24,13 @@ def callback(recognizer, audio):
 
 r = sr.Recognizer()
 m = sr.Microphone()
-#m.SAMPLE_RATE = 48000
 with m as source:
-	r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
+	r.adjust_for_ambient_noise(source)
 
-# start listening in the background (note that we don't have to do this inside a `with` statement)
 stop_listening = r.listen_in_background(m, callback)
-# `stop_listening` is now a function that, when called, stops background listening
 
-# do some other computation for 5 seconds, then stop listening and keep doing other computations
 import time
-for _ in range(50): time.sleep(0.1) # we're still listening even though the main thread is doing other things
+for _ in range(50): time.sleep(0.1)
 for i in range(10**4):
-	print() # calling this function requests that the background listener stop listening
 	time.sleep(1)
 while True: time.sleep(0.1)
