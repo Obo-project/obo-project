6f10f626b83d87b9ae29b04623575da5b58e3d8d
maelforcier <mael.forcier@gmail.com>
2016-05-09 16:12:29 +0200

    Create hasExport.py

---
+++ client/relations/hasExport.py
@@ -0,0 +1,19 @@
+from relation import relation, relationData
+import re
+
+UNIVERSAL = re.compile(r'.*')
+IS_EXPORT = re.compile(r'.*import\bis\b.*')
+S_IS_EXPORT = re.comple(r'.*\'s\bimport.*is.*')
+S_ACCOUNTED_FOR_EXPORT = re.compile(r'.*\'s\bimport.*\baccounted\b.*for.*')
+ACCOUNTED_FOR_EXPORT = re.compile(r'.*import.*accounted\b.*for.*')
+
+
+def make_nice(x):
+    return x
+
+hasImport = relation('hasImport' , 'GPE' , 'CDD' , make_nice , patterns_list=[
+    {'left': UNIVERSAL, 'middle': IS_EXPORT, 'comparator': 'egal'},
+    {'left': UNIVERSAL, 'middle': S_IS_EXPORT, 'comparator': 'egal'},
+    {'left': UNIVERSAL, 'middle': ACCOUNTED_FOR_EXPORT, 'comparator': 'egal'},
+    {'left': UNIVERSAL, 'middle': S_ACCOUNTED_FOR_EXPORT, 'comparator': 'egal'},
+])
