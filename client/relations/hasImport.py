6e61c25d4a7859bdfab47ea22e8c15ed6d093830
maelforcier <mael.forcier@gmail.com>
2016-05-09 16:13:22 +0200

    Update hasImport.py

--- client/relations/hasImport.py
+++ client/relations/hasImport.py
@@ -3,17 +3,17 @@ import re
 
 UNIVERSAL = re.compile(r'.*')
 IS_IMPORT = re.compile(r'.*import\bis\b.*')
-S_IMPORT = re.comple(r'.*\'s\bimport.*is.*')
-S_ACCOUNTED_FOR = re.compile(r'.*\'s\bimport.*\baccounted\b.*for.*')
-ACCOUNTED_FOR = re.compile(r'.*import.*accounted\b.*for.*')
+S_IS_IMPORT = re.comple(r'.*\'s\bimport.*is.*')
+S_ACCOUNTED_FOR_IMPORT = re.compile(r'.*\'s\bimport.*\baccounted\b.*for.*')
+ACCOUNTED_FOR_IMPORT = re.compile(r'.*import.*accounted\b.*for.*')
 
 
 def make_nice(x):
     return x
 
 hasImport = relation('hasImport' , 'GPE' , 'CDD' , make_nice , patterns_list=[
-    {'left': UNIVERSAL, 'middle': IS, 'comparator': 'egal'},
-    {'left': UNIVERSAL, 'middle': S_IS, 'comparator': 'egal'},
-    {'left': UNIVERSAL, 'middle': ACCOUNTED_FOR, 'comparator': 'egal'},
-    {'left': UNIVERSAL, 'middle': S_ACCOUNTED_FOR, 'comparator': 'egal'},
+    {'left': UNIVERSAL, 'middle': IS_IMPORT, 'comparator': 'egal'},
+    {'left': UNIVERSAL, 'middle': S_IS_IMPORT, 'comparator': 'egal'},
+    {'left': UNIVERSAL, 'middle': ACCOUNTED_FOR_IMPORT, 'comparator': 'egal'},
+    {'left': UNIVERSAL, 'middle': S_ACCOUNTED_FOR_IMPORT, 'comparator': 'egal'},
 ])
